5426288b04907681a7b245aef4cdbd286fa1f84d
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-11-06 21:22:42 +0300

    Add UI and update it using redux

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const reducer = (state = 0, action) => {
     switch (action.type) {
         case 'INC':
             return state + 1;
+        case 'DEC':
+            return state - 1;
         default:
             return state;
     }
@@ -12,11 +14,21 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-    console.log(store.getState());
+document.querySelector('#inc').addEventListener('click', () => {
+        store.dispatch({type: 'INC'})
 });
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+
+document.querySelector('#dec').addEventListener('click', () => {
+        store.dispatch({type: 'DEC'})
+});
+
+const update = () => {
+    document.querySelector('#counter').innerHTML = store.getState();
+};
+
+
+store.subscribe(update);
+
 
 
 
